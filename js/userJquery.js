377ad95db8f632916b43fc1cfcef4363c64c881c
GabrielLeandroBS <gabrielleanbs@outlook.com>
2020-11-22 23:50:47 -0300

    Atualizações

--- js/userJquery.js
+++ js/userJquery.js
@@ -2,14 +2,16 @@ $(window).on("load",function(){
 
     $(".addItem").click(function(){
         $(".container-input").css("display","block")
-        $(".container-input").css("zIndex","80")
-        $("section.options").slideToggle();
-    }) 
-/*
+        $("section.options").slideToggle("slow");
+    })  
+    
+    $(".adicionarTarefa").click(function(){
+        $("section.options").slideToggle("slow");
+    })
 
-    $(".options input").blur(function(){
-        $("section.options").slideToggle(800,function(){
-            $(".container-input").css("zIndex","-80")
-        });  
-    })*/
+    $("#group").click(function(){
+        console.log("testans")
+        $(this).addClass("animate__animated animate__bounce")
+    })
+
 })  
