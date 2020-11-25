0363cf89fc96fb4d4515a23397a41458b793d849
GabrielLeandroBS <gabrielleanbs@outlook.com>
2020-11-25 17:59:15 -0300

    Atualzação

--- js/app.js
+++ js/app.js
@@ -4,15 +4,18 @@ var application = angular.module("myList", ["ngStorage"]);
 application.controller("myCtrl", function ($scope) {
   // Criar array para adição de objetos
   $scope.array = [];
+
   // Criamos um evento de clique no HTML chamando essa função
   $scope.addItem = function () {
+
     // Se o input estiver vazio, retorne falso
-    if (!$scope.addItem) return;
+    if (!$scope.addMe) return;
     // Pegamos o valor no console log do IndexOf do AddMe
     // AddMe = value do input
     if ($scope.array.indexOf($scope.addMe) === -1) {
       // Adiciona com o push (empurra um novo valor) para o addMe
       $scope.array.push($scope.addMe);
+
       $scope.save();
       $scope.addMe = "";
     } else {
@@ -21,20 +24,11 @@ application.controller("myCtrl", function ($scope) {
   };
   // Remover item
   $scope.removeItem = function (x) {
-      $scope.array.splice(x, 1)     
-      $scope.save();
+    $scope.array.splice(x, 1);
+    $scope.save();
   };
 
   // Manipulação da data e hora
-  $scope.date = {
-    value: new Date(2020, 8, 20),
-  };
-
-
-
-  $scope.hora = {
-    value: new Date(00, 00, 00),
-  };
 
   // LocalStorage
 
@@ -46,13 +40,10 @@ application.controller("myCtrl", function ($scope) {
     } else {
       $scope.array = JSON.parse(dados);
     }
-
-    
   };
 
   $scope.save = function () {
     localStorage.setItem("lista", JSON.stringify($scope.array));
   };
-
   $scope.pageReload();
 });
--- js/userJquery.js
+++ js/userJquery.js
@@ -1,53 +1,53 @@
-$(".fa-plus").click(function (event) {
-  event.stopPropagation;
-  $(".container-input").css("display", "block");
-  $("section.options").slideToggle("slow", function () {
-    $(".options input").focus(function () {
-      console.log("FOcus ok");
-      $("section.option").css("position", "relative");
+  $(".fa-plus").click(function (event) {
+    event.stopPropagation;
+    $(".container-input").css("display", "block");
+    $("section.options").slideToggle("slow", function () {
+      $(".options input").focus(function () {
+        $("section.option").css("position", "relative");
+      });
     });
-  });
-  $(".options").css("display", "fixed");
-});
-
-$(".adicionarTarefa").click(function () {
-  $("section.options").slideToggle("slow");
-});
-
-
-
-
-function verificar() {
-  $(".button").click(function () {
-    const name = $(".input_name").val();
-    const like = $(".input_like").val();
-    const setName = localStorage.setItem("Nome", name);
-    const setLike = localStorage.setItem("Gosto", like);
-    let verifyName = localStorage.getItem("Nome");
-    let verifyLike = localStorage.getItem("Gosto");
-    if (verifyLike && verifyName) {
-      console.log("verdadeiro");
-      window.location.href = "app.html";
-    } else {
-      console.log("false");
-      console.log(verifyName);
-      return false;
-    }
+    $(".options").css("display", "fixed");
   });
 
-  $(".verifyKey").click(function () {
-    let verifyName = localStorage.getItem("Nome");
-    let verifyLike = localStorage.getItem("Gosto");
-    if (verifyLike && verifyName) {
-      window.location.href = "app.html";
-      document.getElementById("html").innerHTML = "<b>teste</b>";
-    } else {
-      window.location.href = "form.html";
-    }
+  $(".adicionarTarefa").click(function () {
+    $("section.options").slideToggle("slow");
   });
-}
-verificar();
 
-$(".container-itens").dblclick(function(){
-    alert("teste")
-})
+  function verificar() {
+    $(".button").click(function () {
+      const name = $(".input_name").val();
+      const like = $(".input_like").val();
+      const time = $(".input-time").val();
+      const date = $(".input-date").val();
+      const setName = localStorage.setItem("Nome", name);
+      const setLike = localStorage.setItem("Gosto", like);
+      let verifyName = localStorage.getItem("Nome");
+      let verifyLike = localStorage.getItem("Gosto");
+      if (verifyLike && verifyName) {
+        console.log("verdadeiro");
+        window.location.href = "app.html";
+      } else {
+        console.log("false");
+        console.log(verifyName);
+        return false;
+      }
+    });
+
+    $(".verifyKey").click(function () {
+      let verifyName = localStorage.getItem("Nome");
+      let verifyLike = localStorage.getItem("Gosto");
+      if (verifyLike && verifyName) {
+        window.location.href = "app.html";
+        document.getElementById("html").innerHTML = "<b>teste</b>";
+      } else {
+        window.location.href = "form.html";
+      }
+    });
+  }
+  verificar();
+
+  $("body").dblclick(function () {
+    $("section.options").slideToggle("slow");
+  });
+
+ 
